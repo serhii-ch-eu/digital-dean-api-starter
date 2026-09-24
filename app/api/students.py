c7f5e12210cdc6fb9763ec021606e9ac1027f19b
serhii-ch-eu <serhii.choporov@e-u.edu.ua>
2026-09-24 16:24:49 +0300

    Add app/api/students.py

---
+++ app/api/students.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/api/v1/students", tags=["students"])
+
+# TODO 1: реалізуйте POST /api/v1/students.
+# TODO 2: реалізуйте GET /api/v1/students.
+# TODO 3: реалізуйте GET /api/v1/students/{student_id}.
+# TODO 4: після базової версії додайте PATCH /api/v1/students/{student_id}.
+# TODO 5: перетворюйте помилки репозиторію на визначені HTTP-відповіді.
